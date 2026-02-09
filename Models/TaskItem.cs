000185994c6a192c0c97084118da02b880b701bc
Alexander_M <s.medvedev2007@gmail.com>
2026-02-09 16:10:13 +0300

    first commit

---
+++ Models/TaskItem.cs
@@ -0,0 +1,20 @@
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+namespace TaskManagerApi.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public bool IsCompleted { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using TaskManagerApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+//тут закончил работу
